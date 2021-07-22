96355cf04e52e56e8c2283e43ceb21453012740c
Ryan Yu <ryanyu@MacBook-Pro.fios-router.home>
2021-07-22 17:09:48 -0400

    Mat's stuff

--- hello.py
+++ hello.py
@@ -1,2 +1,12 @@
-print("Hello World.")
+from Eir import SIR
 
+beta = 1.2
+gamma = .15
+S0 = 999999
+I0 = 1
+R0 = 0
+
+sim = SIR(beta=beta, gamma=gamma, S0=S0, I0=I0, R0=R0)
+df, fig = sim.run(days=31, dt=0.1)
+
+print(df)
--- main.java
+++ main.java
@@ -1,7 +1,28 @@
-public class main{
+public class Main{
 	public static void main(String[] args)
 {
-	System.out.print("Hello world, I finally figured out M1.");
+	System.out.println("Hello world, I literally finally figured out M1 you son of a bitch.");
+	System.out.println("Starting to print numbers: ");
+	int[] testArr = {1,3,5,6,8};
+	Main.reverse(testArr);
+	for(int i: testArr)
+	{
+		System.out.print(i + " ");
+	}
+}
+
+public static void reverse(int[] arr)
+{
+	int begin = 0;
+	int end = arr.length - 1;
+	while(begin <= end)
+	{
+		int temp = arr[begin];
+		arr[begin] = arr[end];
+		arr[end] = temp;
+		begin++;
+		end--;
+	}
 }
 }
 
